a1c4b2406ec46bb54649644f59918c357e0e25c5
Ray Pulsipher <ray@cmagic.biz>
2019-01-17 13:07:41 -0800

    - Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # wsl_wiki
 Code to help convert the WA State Library wiki resources to run offline
+
+# Purpose
+The Washington State Library has put together a great wiki full or resources that can be used
+for reentry by inmates. Due to a lack of internet access by inmates, this is an attempt to
+automate the conversion of the wiki to run fully offline in a prison environment.
+
+# Current Status
+BETA - Lots of bugs in the code, but it works as a first draft. Pulled 688 pdf files on current run.
+
+# Features
+- Pulls PDF files that are linked externally and makes a copy on the local server.
+- Pulls external HTML pages and renders them as a PDF file - stores on the local server.
+- Updates links in the wiki to point to local server.
+
+# Environment
+- Windows - running under XAMP with bitnami media wiki. Good for putting on WSL laptops.
+- Planned (not available yet) - Run as a docker container to become part of the OPE project.
+
+# Windows Setup
+1) Install XAMP - Pull PHP 7.3.? version - https://www.apachefriends.org/download.html
+    - Only need Apache/Mysql running.
+2) Install Bitnami MediaWiki - Get Windows installer - https://bitnami.com/stack/mediawiki/installer
+    - Install w default options
+3) Stop XAMP (stop apache, mysql, close XAMP management app)
+4) Copy in zipped XAMP folder (ask rpulsipher@pencol.edu - includes secrets right now).
+5) Start up XAMP/Apache/Mysql
+6) Navigate to http://localhost:8080/mediawiki
+
+# Development
+To run the python script which will download/fix links, you need python 3.6.? installed (python.org). Look at modules.txt in this folder for the modules you need to install.
+
+- Install python modules (e.g.  pip install pdfkit)
+
+- Run py file in the c:\xamp\apps\mediawiki\htdocs folder
+    - cd c:\xamp\apps\mediawiki\htdocs
+    - python find_links.py
+    - NOTE - buts - may need to delete links.db and dl_files on subsequent runs if things quit working and let it start fresh.
---
+++ find_links.py
@@ -0,0 +1,174 @@
+
+import os
+import sys
+import json
+import requests
+import hashlib
+import shutil
+import re
+import pdfkit
+import pdb
+
+from sqlitedict import SqliteDict
+import pymysql.cursors
+import pymysql
+
+mysql_server = "localhost"
+mysql_port = 3306
+mysql_db = "bitnami_mediawiki"
+mysql_user = "wsl_offline"
+mysql_pass = "WSL_WIKI_OFFLINE"
+
+local_server = "http://localhost:8080/mediawiki/"
+
+
+def get_app_path():
+    return os.path.abspath(os.path.dirname(__file__))
+
+pdf_config = pdfkit.configuration(wkhtmltopdf=os.path.join(get_app_path(), 'wkhtmltopdf.exe'))
+    
+def get_file_name_from_cd_header(cd_header):
+    # Pull the filename from the content-disposition header
+    if not cd_header:
+        return ""
+    fname = re.findall('filename=(.+)', cd_header)
+    if len(fname) == 0:
+        return ""
+    return fname[0]
+    
+    
+def download_file(url, local_path):
+    local_file_name = local_path
+    # print("   " + url + " -- " + local_path)
+    # pdb.set_trace()
+    r = requests.get(url, stream=True, allow_redirects=True)
+    if r.status_code == 404:
+        print(" ---- 404 URL Not Found: " + url)
+        return None
+    fname = get_file_name_from_cd_header(r.headers.get('content-disposition'))
+    content_type = r.headers.get('content-type')
+    extension = ".html"
+    if 'text/html' in content_type:
+        # Use pdfkit to get a pdf version of the page
+        pdfkit.from_url(url, local_path)
+        # Should be a pdf now
+        extension = ".pdf"
+    elif '/pdf' in content_type:
+        extension = ".pdf"
+        # print("     " + fname + " / " + content_type)
+        with open(local_file_name, "wb") as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                if chunk:  # filter out keep-alive empty chunks
+                    f.write(chunk)
+                    # f.flush()
+    return extension
+    
+# Local db connection with list of links
+linkdb = SqliteDict(get_app_path() + "/links.db",
+                    encode=json.dumps, decode=json.loads,
+                    autocommit=False)
+
+# Connect to mysql database to find links
+mysqldb = pymysql.connect(host=mysql_server, user=mysql_user, password=mysql_pass,
+                            db=mysql_db, charset='utf8mb4',
+                            cursorclass=pymysql.cursors.DictCursor)
+                            
+
+with mysqldb.cursor() as cursor:
+    # Pull links from the wiki database
+    sql = "SELECT * FROM externallinks"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        k = row["el_to"]
+        if k not in linkdb:
+            print("Found new link... {0}".format(k))
+            linkdb[k] = ""
+            
+        #print(" To {0} Index {1}".format(row["el_to"], row["el_index"]))
+    linkdb.commit()
+    
+    # Now download each file
+    base_dl_path = os.path.join(get_app_path(), "dl_files")
+    try:
+        os.makedirs(base_dl_path)
+    except:
+        pass
+    
+    link_count = len(linkdb)
+    
+    link_index = 0
+    # See if each file exists in the dl_path
+    for key, value in linkdb.iteritems():
+        link_index += 1
+        try:
+            k_url = key.decode("utf-8")
+            v = value
+            if k_url == "":
+                print("URL Missing: " + k_url)
+                continue
+            
+            if len(v) > 10:
+                print("File already downloaded: " + k_url)
+                continue
+            
+            print("Downloading File: " + str(link_index) + "  " + k_url)
+            tmp_file = os.path.join(base_dl_path, "tmp_dl")
+            
+            extension = download_file(k_url, tmp_file)
+            if extension is None:
+                # Unable to dl file, move to next one
+                continue
+            # Calculate tmp file hash
+            h = hashlib.sha1()
+            with open(tmp_file, "rb") as f:
+                chunk = f.read(65535)
+                if not chunk:
+                    continue
+                h.update(chunk)
+            hash = h.hexdigest()
+            final_path = os.path.join(base_dl_path, hash + extension)
+            shutil.copyfile(tmp_file, final_path)
+            if hash != "":
+                linkdb[key] = hash + extension
+            
+            # Update mysql database with new link
+            new_url = local_server + "dl_files/" + hash + extension
+            #sql = "UPDATE externallinks SET el_to=%s WHERE el_to=%s"
+            #cursor.execute(sql, (new_url ,k_url))
+            #mysqldb.commit()
+            sql = "UPDATE text SET old_text=REPLACE(old_text, %s, %s)"
+            cursor.execute(sql, (k_url, new_url))
+            mysqldb.commit()
+        except Exception as ex:
+            print("Error pulling file: " + str(ex))
+            continue
+            
+        linkdb.commit()
+    print(base_dl_path)
+    
+    # Need to get a list of pages and purge them all so wiki text refreshes
+    sql = "SELECT page_title FROM page"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    
+    for row in rows:
+        title = row["page_title"].decode("utf-8")
+        url = local_server + "index.php/" + title
+        params = dict()
+        params["action"] = "purge"
+        # Purge the page to make it refresh from the database
+        r = requests.post(url, params=params, allow_redirects=True)
+        print("Purging Page: " + title + " - " + url + " " + str(r.status_code))
+        
+    # http://localhost:8080/mediawiki/index.php/Benton_County
+    # http://example.org/wiki/index.php?title=Main_Page&action=purge
+    # c:\xampp\php\php.exe purgePage.php
+    
+    
+linkdb.commit()
+linkdb.close()        
+mysqldb.close()
+
+
