5665e1741034377d072f2ffff22f45e2d72a158e
Jeremy Stroud <jeremy.stroud@sos.wa.gov>
2019-08-14 11:58:51 -0700

    Added comments to config-dist

--- config-dist.py
+++ config-dist.py
@@ -1,3 +1,8 @@
+# server - MySQL database hose
+# port - MySQL server port
+# db - Wiki database name
+# user - Wiki database username
+# pass - Wiki database password
 mysql = {
     "server": "localhost",
     "port": 3306,
@@ -6,4 +11,7 @@ mysql = {
     "pass": "pass",
 }
 
+# url - Wiki website address
+# path - Absolute path to Wiki website root - Windows users must use double-backslashes (\\) in the path
 wiki = {"url": "http://localhost/", "path": "/var/www/html/"}
+
