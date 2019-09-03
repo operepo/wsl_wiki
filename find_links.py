8dec457b441f46c04ff83498bc82d8ee087834d2
Jeremy Stroud <jeremy.stroud@sos.wa.gov>
2019-09-03 09:58:05 -0700

    Multithreaded, reorganized functions

--- find_links.py
+++ find_links.py
@@ -12,158 +12,179 @@ import pymysql
 import config as cfg
 from ThreadPool import ThreadPool
 
+
 # Global variables
 dl_path = ""
-# thread_pool = ThreadPool(4)
-timeout = 30.00  # Allows 30 seconds for page load/timeout
+thread_pool = ThreadPool(10)
+file_cleanup = []
+max_items = 0        # Max number of entries to process, 0 for unlimited -- leave at 0 except for testing
+
+def main():
+    print("******* Converting links to offline *******")
+
+    # Initilize globals
+    set_pdfkit_config()
+    set_dl_path()
+    linkdb = get_linkdb()
+    wikidb = get_wikidb()
+
+    externallinks = get_externallinks(wikidb.cursor())
+
+    # iterate through externallinks, add to linkdb if new
+    for link in externallinks:
+        k = link["el_to"].decode("utf8")
+        if k not in linkdb:
+            print("Found new link... " + k)
+            linkdb[k] = ""
+
+    linkdb.commit()
+
+    # See if each file exists in the dl_path
+    i = 0
+    for key, value in linkdb.iteritems():
+        if i > max_items:
+            break
+        else:
+            if max_items > 0:
+                i += 1
+
+        if key == "":
+            print("URL Missing: %s." % key)
+            continue
+
+        thread_pool.add_task(get_docs, key, value)
+
+    thread_pool.wait_completion()
+    purge_pages()
+    linkdb.commit()
+    cleanup_files()
+    
+    linkdb.close()
+    wikidb.close()
+    print("Complete. Files downloaded to " + dl_path)
 
 
-def download_file(url, local_path):
+def cleanup_files():
+    for item in file_cleanup:
+        try:
+            # Check if file still being referred to in linkdb
+            if not item in linkdb.values():
+                os.remove(dl_path + item + ".pdf")
+        except:
+            # File not found
+            pass    
+
+
+def download_file(url, old_hash = ""):
+    # Downloads file from URL, if old_hash is provided, compares file hashes to determine if the file should be re-downloaded or not
+    # Returns new_hash
+    global file_cleanup
+    timeout = (5, 30)  # Allows 5 seconds to connect, 30 seconds for page load/timeout
+
     try:
-        print("Downloading File: %s to %s." % (url, local_path))
         headers = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"
         }
         r = requests.get(
             url, stream=True, allow_redirects=True, headers=headers, timeout=timeout
         )
-        if r.status_code == 404:
-            print(" ---- 404 URL Not Found: " + url)
-            with open("404errors.log", "a") as f:
-                print(url, file=f)
-            return None
-        content_type = r.headers.get("content-type")
-        extension = None
-        if "text/html" in content_type:
-            # Use pdfkit to get a pdf version of the page
-            options = {"log-level": "error"}
-            pdfkit.from_url(url, local_path, options=options)
-            extension = ".pdf"
-        elif "/pdf" in content_type:
-            extension = ".pdf"
-            with open(local_path, "wb") as f:
-                for chunk in r.iter_content(chunk_size=1024):
-                    if chunk:  # filter out keep-alive empty chunks
-                        f.write(chunk)
-                        # f.flush()
-        return extension
-    except requests.exceptions.Timeout as ex:
-        print("Timeout downloading file: %s | %s" % (url, str(ex)))
+
+        if r.status_code != 200:
+            raise Exception("%s\t%s" % (r.status_code, url))
+
+        h = hashlib.sha1()
+        h.update(r.content)
+        new_hash = h.hexdigest()
+
+        if old_hash != new_hash:
+            file_cleanup.append(old_hash)
+            content_type = r.headers.get("content-type")
+            if "text/html" in content_type:
+                # Use pdfkit to get a pdf version of the page
+                options = {
+                    "log-level": "none",
+                    "load-error-handling": "ignore",
+                    "load-media-error-handling": "ignore",
+                    "disable-external-links": False,
+                    "disable-internal-links": False,
+                    "disable-javascript": False,
+                }
+                #pdfkit.from_url(url, new_hash, options=options)
+                try:
+                    pdfkit.from_string(r.content.decode("utf8"), new_hash, options=options)
+                except:
+                    # pdfkit errors can be ignored, file is still generated
+                    pass
+            elif "/pdf" in content_type:
+                with open(new_hash, "wb") as f:
+                    for chunk in r.iter_content(chunk_size=1024):
+                        if chunk:  # filter out keep-alive empty chunks
+                            f.write(chunk)
+        return new_hash
     except Exception as ex:
-        print("Error downloading file: " + str(ex))
-
-
-def find_links():
-    print("******* Converting links to offline *******")
-
-    # Initilize
-    set_pdfkit_config()
-    set_dl_path()
-
-    # Local db connection with list of links
-    linkdb = get_linkdb()
-
-    # Connect to mysql database to find links
-    mysqldb = get_wikidb()
-
-    with mysqldb.cursor() as cursor:
-        externallinks = get_externallinks(cursor)
-
-        # iterate through externallinks, add to linkdb if new
-        for link in externallinks:
-            k = link["el_to"]
-            if k not in linkdb:
-                print("Found new link... " + str(k))
-                linkdb[k] = ""
-
-        linkdb.commit()
-
-        # See if each file exists in the dl_path
-        for key, value in linkdb.iteritems():
-            k_url = key.decode("utf-8")
-
-            try:
-                if k_url == "":
-                    print("URL Missing: %s." % k_url)
-                    continue
-
-                if len(value) > 10:
-                    print("File already downloaded: %s." % k_url)
-                    continue
-
-                tmp_file = os.path.join(dl_path, "tmp_dl")
-
-                extension = download_file(k_url, tmp_file)
-                if extension is None:
-                    # Unable to dl file, move to next one
-                    continue
-
-                # Calculate tmp file hash
-                h = hashlib.sha1()
-
-                with open(tmp_file, "rb") as f:
-                    chunk = f.read(65535)
-                    if not chunk:
-                        continue
-                    h.update(chunk)
-                hash = h.hexdigest()
-                final_path = os.path.join(dl_path, hash + extension)
-
-                shutil.copyfile(tmp_file, final_path)
-
-                if hash != "":
-                    linkdb[key] = hash + extension
-
-                # Update mysql database with new link
-                new_url = cfg.wiki["url"] + "dl_files/" + hash + extension
-
-                print("File download complete: %s to %s" % (k_url, new_url))
-
-                sql = "UPDATE text SET old_text=REPLACE(old_text, %s, %s);"
-                cursor.execute(sql, (k_url, new_url))
-                mysqldb.commit()
-            except Exception as ex:
-                print("Error pulling file: " + str(ex))
-                continue
-
-            linkdb.commit()
-
-    # thread_pool.wait_completion()
-    print("Files downloaded to " + dl_path)
-    purge_pages()
-    linkdb.commit()
-    linkdb.close()
-    mysqldb.close()
+        with open("errors.log", "a") as f:
+            print("%s\t%s\t%s" % (r.status_code, url, str(ex)), file=f)
+        return ""
 
 
 def get_app_path():
     return os.path.abspath(os.path.dirname(__file__))
 
 
+def get_docs(url, old_hash):
+    try:
+        linkdb = get_linkdb()
+        mysqldb = get_wikidb()
+
+        # Fetch and process documents
+        file_hash = download_file(url, old_hash)
+        linkdb[url] = file_hash
+        linkdb.commit()
+
+        if file_hash == "":
+            # File not downloaded, continue to next item
+            return
+
+        final_path = os.path.join(dl_path, file_hash + ".pdf") # All files are converted to PDF
+
+        shutil.move(file_hash, final_path)
+
+        # Update mysql database with new link
+        new_url = cfg.wiki["url"] + "dl_files/" + file_hash + ".pdf"
+        
+        # sql = "UPDATE text SET old_text=REPLACE(old_text, %s, %s);"  # was errantly replacing substrings
+        data = [
+            ("[" + url + "]", "[" + new_url + "]"),
+            ("[" + url + "|", "[" + new_url + "|")
+        ]
+        sql = "UPDATE text SET old_text=REPLACE(old_text, %s, %s);"
+        
+        result = mysqldb.cursor().executemany(sql, data)
+        
+        with open("db.log", "a") as d:
+            print("Number of rows affected by statement: %d - %s : %s" % (result, url, new_url), file=d)
+
+        mysqldb.commit()
+        print("Downloaded %s to %s" % (url, new_url))
+    except Exception as ex:
+        print(str(ex))
+
+
 def get_externallinks(cursor):
     # Pull links from the wiki database
     try:
-        sql = "SELECT * FROM externallinks"
+        sql = "SELECT DISTINCT `el_to` FROM externallinks ORDER BY `el_to` DESC;"
         cursor.execute(sql)
         return cursor.fetchall()
     except Exception as ex:
         sys.exit("Error retrieving external links: " + str(ex))
 
 
-def get_file_name_from_cd_header(cd_header):
-    # Pull the filename from the content-disposition header
-    if not cd_header:
-        return ""
-    fname = re.findall("filename=(.+)", cd_header)
-    if len(fname) == 0:
-        return ""
-    return fname[0]
-
-
 def get_linkdb():
     try:
+        # get_app_path() + "/links.db",
         return sqlitedict.SqliteDict(
-            get_app_path() + "/links.db",
+            "links.db",
             encode=json.dumps,
             decode=json.loads,
             autocommit=False,
@@ -227,8 +248,4 @@ def set_pdfkit_config():
 
 
 # Call main program
-# http://localhost:8080/mediawiki/index.php/Benton_County
-# http://example.org/wiki/index.php?title=Main_Page&action=purge
-# c:\xampp\php\php.exe purgePage.php
-
-find_links()
+main()
